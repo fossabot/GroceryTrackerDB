12b6499b92b044876ed4e889b229f40dcb734658
Andrew Ingson <aings1@umbc.edu>
2020-10-10 14:26:22 -0400

    Fleshed out settings page a bit

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -2,22 +2,39 @@ import * as React from 'react';
 import {StyleSheet} from 'react-native';
 
 import {Text, View} from '../components/Themed';
+import ToggleSwitch from "../components/settings/toggle_switch";
 
 export default function SettingsScreen() {
   return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Settings</Text>
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
-        <Text style={styles.body}>This page will have app settings</Text>
+      <View style={styles.main_container}>
+        <View style={styles.heading_container}>
+          <Text style={styles.title}>Settings</Text>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
+        </View>
+        <View style={styles.body_container}>
+          <Text style={styles.body}>This page will have app settings</Text>
+          <ToggleSwitch/>
+          <Text style={styles.body}> Default Scanner to Camera On</Text>
+        </View>
       </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main_container: {
     flex: 1,
+  },
+  heading_container: {
+    flex: 1,
+    height: 100,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  body_container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
   title: {
     fontSize: 20,
@@ -25,7 +42,6 @@ const styles = StyleSheet.create({
   },
   body: {
     fontSize: 14,
-    justifyContent: 'center',
   },
   separator: {
     marginVertical: 30,
